349844d40aef06694b1be59c8c31f0cf22e8626c
yeniirahmaa512 <yenirahma396@gmail.com>
2025-05-06 08:58:15 +0700

    revisi jenis tulisan

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,10 +6,11 @@
     <title>Beauty Salon</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500;700&family=Poppins:wght@300;400;600&display=swap" rel="stylesheet">
 
     <style>
         body {
-            font-family: 'Segoe UI', sans-serif;
+            font-family:  'Poppins', sans-serif;
             background: linear-gradient(to right, #f3e7e9, #e3eeff);
             margin: 0;
             padding: 0;
