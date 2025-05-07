c2409715b6bec113be3b75284b26448ea84493fc
unknown <lucastatag2006@gmail.com>
2025-05-07 14:37:42 +0700

    merapikan layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,7 @@
 
     <title>Salon Beauty Clone</title>
 
-    <title>Beauty Salon</title>
+    <title>Beauty </title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
 
