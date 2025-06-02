0436c5db4a99e5f68c6c6d8e5bd11d60cdac8bf9
yeniirahmaa512 <yenirahma396@gmail.com>
2025-06-02 13:03:23 +0700

    menambahkan halaman

--- resources/views/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['admin'])) header("Location: index.php");
-include 'db.php';
-
-$id = $_GET['id'];
-mysqli_query($conn, "DELETE FROM salons WHERE id=$id");
-header("Location: salons.php");
